b074e120b9ca9a9bd28f3d3aed45de80198de5f3
Simon :) <82384235+smn-t@users.noreply.github.com>
2024-11-20 09:40:26 +0100

    Update notify.py

--- scripts/notify.py
+++ scripts/notify.py
@@ -21,8 +21,8 @@ time_series_daily = data.get("Time Series (Daily)", {})
 # Convert the extracted data to a DataFrame
 df = pd.DataFrame.from_dict(time_series_daily, orient='index')
 df.sort_index(inplace=True)
-df['4. close'] = df['4. close'].astype(float)
-df['200_sma'] = df['4. close'].rolling(window=200).mean()
+df['4. close'] = df['4. close'].astype(float).round(2)
+df['200_sma'] = df['4. close'].rolling(window=200).mean().round(2)
 
 bot = telegram.Bot(token=TELEGRAM_TOKEN)
 
@@ -30,7 +30,7 @@ genai.configure(api_key=GEMINI_TOKEN)
 model = genai.GenerativeModel("gemini-1.5-pro")
 prompt = r'''
 Schreibe eine kurze, präzise, lockere Telegramm-Chat-Nachricht an die Investoren.
-Schreibe wie ein risikofreudiger Nutzer des Subreddits "Mauerstrassenwetten"
+Schreibe im Stil der göttlichen Komödie.
 Benutze hierbei die gegeben Zeitreihe, vergleiche den Kurs mit dem sma200, gib ein Update wie der Titel "Amundi ETF Leveraged MSCI USA Daily UCITS ETF EUR" aktuell performt und gib die Änderung zum Vortag in % an.
 Nenne aktuell Werte und gib eine Prognose ab.
 Die Zeitreihe: {0}
