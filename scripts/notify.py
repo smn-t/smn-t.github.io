55730952d9d3d80265de44f74a797e5c161a1eb4
Simon :) <82384235+smn-t@users.noreply.github.com>
2024-11-28 20:18:27 +0100

    Update notify.py

--- scripts/notify.py
+++ scripts/notify.py
@@ -29,9 +29,9 @@ bot = telegram.Bot(token=TELEGRAM_TOKEN)
 genai.configure(api_key=GEMINI_TOKEN)
 model = genai.GenerativeModel("gemini-1.5-pro")
 prompt = r'''
-Schreibe eine kurze, präzise, lockere Telegramm-Chat-Nachricht über den Kurs des ETFs.
+Schreibe eine möglichst depressive Telegramm-Chat-Nachricht über den Kurs des ETFs.
 Benutze hierbei die gegeben Zeitreihe, vergleiche den Kurs mit dem sma200, gib ein Update wie der Titel "Amundi ETF Leveraged MSCI USA Daily UCITS ETF EUR" aktuell performt und gib die Änderung zum Vortag in % an.
-Nenne aktuell Werte und gib eine Prognose ab. Die Trading Strategie ist der simple moving average über 200 Tage, ordne das Signal ein.
+Nenne den aktuellen Wert und den Wert vom Vortag und gib eine Prognose ab. Die Trading Strategie ist der simple moving average über 200 Tage, ordne das Signal ein.
 Die Zeitreihe: {0}
 Der SMA200: {1}
 Heute ist der {2}'''.format(df['4. close'].tail(100).to_string().replace(".", ","), df['200_sma'].tail(100).to_string().replace(".", ","), df['4. close'].tail(1).index)
